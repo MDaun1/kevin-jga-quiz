ab21d4116f3c9b3324abd29a0e55e00de10c5f44
MDaun1 <84075731+MDaun1@users.noreply.github.com>
2026-08-18 18:50:17 +0200

    Update questions.js

--- src/questions.js
+++ src/questions.js
@@ -1,116 +1,102 @@
-const questions = [
-  {
-    question: "Welche sind Jackys Lieblingshelden in Overwatch?",
-    answers: ["D.Va (Tank)", "Sierra (DD)", "Lifeweaver (Heal)"],
-    correct: 0,
-    category: "Gaming"
-  },
-  {
-    question: "Wer ist der bessere Autofahrer?",
-    answers: ["Jacky natürlich 😂", "Kevin"],
-    correct: 0,
-    category: "Beziehung"
-  },
-  {
-    question: "Wo haben sich Jacky und Kevin das erste Mal geküsst?",
-    answers: ["Bei Kevin auf dem Sofa", "Im Auto", "Im Park"],
-    correct: 0,
-    category: "Beziehung"
-  },
-  {
-    question: "Wie lautet das Datum des Jahrestags?",
-    answers: ["05.07.2023", "07.05.2023", "05.06.2023"],
-    correct: 0,
-    category: "Beziehung"
-  },
-  {
-    question: "Wie lautet Jackys Lieblingsessen?",
-    answers: ["Pommes", "Pizza", "Sushi"],
-    correct: 0,
-    category: "Persönlich"
-  },
-  {
-    question: "Wie lautet Jackys Lieblingsfarbe?",
-    answers: ["Rosa (wieder dank Kevin)", "Blau", "Schwarz"],
-    correct: 0,
-    category: "Persönlich"
-  },
-  {
-    question: "Wie war Jackys Abiturschnitt?",
-    answers: ["2,1", "1,9", "2,4"],
-    correct: 0,
-    category: "Persönlich"
-  },
-  {
-    question: "Welche Augenfarbe hat Jacky?",
-    answers: ["Grün-blau", "Braun", "Grau"],
-    correct: 0,
-    category: "Persönlich"
-  },
-  {
-    question: "Wie viele alkoholische Getränke braucht Jacky um voll zu sein?",
-    answers: ["3-Gin-Jacky 😂", "5 Bier", "2 Cocktails"],
-    correct: 0,
-    category: "Spaß"
-  },
-  {
-    question: "Wie viele Stunden hat Jacky in WoW?",
-    answers: ["11.500", "8.000", "5.000"],
-    correct: 0,
-    category: "Gaming"
-  },
-  {
-    question: "Welchen Film könnte Jacky immer wieder schauen?",
-    answers: ["Harry Potter", "Star Wars", "Herr der Ringe"],
-    correct: 0,
-    category: "Filme"
-  },
-  {
-    question: "Was war Kevins erstes Geschenk an Jacky?",
-    answers: ["Halskette in Disney-Schrift", "Blumen", "Schokolade"],
-    correct: 0,
-    category: "Beziehung"
-  },
-  {
-    question: "Wie heißen Jackys zwei Lieblingsbücher?",
-    answers: ["Gut gegen Nordwind & Alle sieben Wellen", "Harry Potter", "Percy Jackson"],
-    correct: 0,
-    category: "Bücher"
-  },
-  {
-    question: "Wie heißt Ruffys Teufelsfrucht?",
-    answers: ["Mensch-Mensch Frucht Modell Nika", "Gum-Gum Frucht", "Feuer-Frucht"],
-    correct: 0,
-    category: "One Piece"
-  },
-  {
-    question: "Wie viele Meter über dem Meer liegt Skypia?",
-    answers: ["10.000m", "5.000m", "20.000m"],
-    correct: 0,
-    category: "One Piece"
-  },
-  {
-    question: "Welche Melodie hört man bei Gear 5?",
-    answers: ["Trommeln der Freiheit", "Himmelsmelodie", "Nika Theme"],
-    correct: 0,
-    category: "One Piece"
-  },
-  {
-    question: "Welche Nummer hat Mew?",
-    answers: ["151", "150", "152"],
-    correct: 0,
-    category: "Pokemon"
-  },
-  {
-    question: "Wie heißt der Staplerfahrer bei Maxion?",
-    answers: ["Tigerente", "Panther", "Bär"],
-    correct: 0,
-    category: "Arbeit"
-  },
-  {
-    question: "Was hat Kevin im Portugalurlaub besonders geschmeckt?",
-    answers: ["Cocktail aus dem COCKTOPF", "Pizza", "Bier"],
-    correct: 0,
-    category: "Urlaub"
-  }
-];
+const questions = {
+  Beziehung: [
+    {
+      question: "Wer ist der bessere Autofahrer?",
+      answer: "Jacky natürlich 😂"
+    },
+    {
+      question: "Wo haben sich Jacky und Kevin das erste Mal geküsst?",
+      answer: "Bei Kevin auf dem Sofa"
+    },
+    {
+      question: "Wie lautet das Datum des Jahrestags?",
+      answer: "05.07.2023"
+    },
+    {
+      question: "Was war Kevins erstes Geschenk an Jacky?",
+      answer: "Eine Halskette in Disney-Schrift"
+    }
+  ],
+
+  Persönlich: [
+    { question: "Wie lautet Jackys Lieblingsessen?", answer: "Pommes" },
+    { question: "Wie lautet Jackys Lieblingsfarbe?", answer: "Rosa (wieder dank Kevin)" },
+    { question: "Wie war Jackys Abiturschnitt?", answer: "2,1" },
+    { question: "Welche Augenfarbe hat Jacky?", answer: "Grün-blau" },
+    { question: "Wie viele alkoholische Getränke braucht Jacky?", answer: "3-Gin-Jacky 😂" }
+  ],
+
+  Gaming: [
+    {
+      question: "Welche sind Jackys Lieblingshelden in Overwatch?",
+      answer: "D.Va (Tank), Sierra (DD), Lifeweaver (Heal)"
+    },
+    {
+      question: "Wie viele Stunden hat Jacky in WoW?",
+      answer: "Ca. 11.500 Stunden"
+    }
+  ],
+
+  Filme: [
+    { question: "Welchen Film könnte Jacky immer wieder schauen?", answer: "Harry Potter" }
+  ],
+
+  Bücher: [
+    {
+      question: "Wie heißen Jackys zwei Lieblingsbücher?",
+      answer: "Gut gegen Nordwind & Alle sieben Wellen"
+    }
+  ],
+
+  OnePiece: [
+    {
+      question: "Wie heißt Ruffys Teufelsfrucht?",
+      answer: "Mensch-Mensch Frucht Modell Nika"
+    },
+    {
+      question: "Wie viele Meter über dem Meer liegt Skypia?",
+      answer: "10.000m"
+    },
+    {
+      question: "Welche Melodie hört man bei Gear 5?",
+      answer: "Trommeln der Freiheit"
+    }
+  ],
+
+  Pokemon: [
+    {
+      question: "Welche Nummer hat Mew?",
+      answer: "151"
+    }
+  ],
+
+  Arbeit: [
+    {
+      question: "Wie heißt der Staplerfahrer bei Maxion?",
+      answer: "Tigerente"
+    }
+  ],
+
+  Urlaub: [
+    {
+      question: "Was hat Kevin im Portugalurlaub besonders geschmeckt?",
+      answer: "Cocktail aus dem COCKTOPF"
+    }
+  ],
+
+  Spaß: [
+    {
+      question: "Was haben Kevin und Mike nach einem Unfall im Auto vergessen?",
+      answer: "Lukas"
+    },
+    {
+      question: "In welchem Fitnessstudio waren Kevin und Mike zuerst?",
+      answer: "Sportpark Siebengebirge"
+    },
+    {
+      question: "Wie ist der Spitzname von Mikes Ehefrau?",
+      answer: "Die bezahlte Schauspielerin"
+    }
+  ]
+};
+
